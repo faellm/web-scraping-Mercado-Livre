ef6065b3528fd21bf7aadc37e4ce72ac8d4121f4
faellm <rafaellara1405@gmail.com>
2022-12-01 09:30:20 -0300

    'ajuste de findAll para trazer todos os produtos'

--- mercado_livre.py
+++ mercado_livre.py
@@ -5,6 +5,7 @@ from time import sleep
 compra = input('O que deseja cotar? ')
 
 sleep(3)
+
 url_base = 'https://lista.mercadolivre.com.br/'
 
 url = (url_base+compra)
@@ -20,23 +21,27 @@ class Buscar():
         site = BeautifulSoup(resposta.text, 'html.parser')
     
         #div com o produto
-        produto = site.find('div', attrs={'class': 'ui-search-result__wrapper shops__result-wrapper'})
-    
-        #titulo do produto, com o nome
-        #erro ao imprimir
-        titulo = produto.find('h2', attrs={'class': 'ui-search-item__title shops__item-title'})
+        produtos = site.findAll('div', attrs={'class': 'ui-search-result__wrapper shops__result-wrapper'})
+
+        for produto in produtos:
         
+            #titulo do produto, com o nome
+            #erro ao imprimir
+            titulo = produto.find('h2', attrs={'class': 'ui-search-item__title shops__item-title'})
+            
+            
+            #selecionando apenas o valor em rais
+            reais = produto.find('span', attrs={'class': 'price-tag-text-sr-only'})
+            #centavos = produto.find('span', attrs={'class': ''})
         
-        #selecionando apenas o valor em rais
-        reais = produto.find('span', attrs={'class': 'price-tag-text-sr-only'})
-        #centavos = produto.find('span', attrs={'class': ''})
-    
-        #selecionando o link em uma variavel(link)
-        link = produto.find('a', attrs={'class':'ui-search-link'})
-    
-        print('Produto:', titulo.text)
-        print('Valor do produto: R$',reais.text )
-        print('Link da pagina:',link['href'])
+            #selecionando o link em uma variavel(link)
+            link = produto.find('a', attrs={'class':'ui-search-link'})
+        
+            print('Produto:', titulo.text)
+            print('Valor do produto: R$',reais.text )
+            print('Link da pagina:',link['href'])
+            
+            print('\n\n')
     
     get_text()
     
